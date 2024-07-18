0c907e4eb37f5af874ebcea3bb521bd18db2c24d
Anjishnu06 <73886177+Anjishnu06@users.noreply.github.com>
2024-07-18 22:48:01 +0530

    Update index.jsx

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -5,10 +5,11 @@ const Home = () => {
   const { currentUser } = useAuth();
   return (
     <div className="text-2xl font-bold pt-14">
+      <center><h1>Report Analysis of Forest Cover</h1></center>
       <iframe
         title="forestcoveranalysis"
-        width="1140"
-        height="541.25"
+        width="1540"
+        height="1041.25"
         src="https://app.powerbi.com/reportEmbed?reportId=8c001da3-bde2-4e3a-807e-b1850323f572&autoAuth=true&ctid=1c87fdb6-051d-43a4-a73c-ad6de4fac5b2"
         frameborder="0"
         allowFullScreen="true"
